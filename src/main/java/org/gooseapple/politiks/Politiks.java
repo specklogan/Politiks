3f4746e6a92297adc9532bc57cd261207eaa73f4
Logan <specklogan@hotmail.com>
2025-02-19 21:43:55 -0600

    Initial Rewrite

---
+++ src/main/java/org/gooseapple/politiks/Politiks.java
@@ -0,0 +1,17 @@
+package org.gooseapple.politiks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Politiks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
